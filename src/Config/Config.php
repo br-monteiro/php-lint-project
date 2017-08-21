18df5bb42995039a1a68d066113915863df43feb
Edson Monteiro <edson.monteiro@chaordicsystems.com>
2017-08-21 18:16:59 -0300

    Adding file to be used for config the application

---
+++ src/Config/Config.php
@@ -0,0 +1,27 @@
+<?php
+namespace KR04\Config;
+
+class Config
+{
+
+    /**
+     * The directory separator
+     */
+    const DS = DIRECTORY_SEPARATOR;
+
+    /**
+     * Directory of entry to scout
+     */
+    const ROOT_DIRECTORY = 'filesTest' . self::DS;
+
+    /**
+     * Extensions considered for the application
+     */
+    const ALLOWED_EXTENSION = ['php'];
+
+    /**
+     * Reach to be shown in bugs
+     */
+    const REACH_DISPLAY = 3;
+
+}
