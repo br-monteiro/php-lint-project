521a24d4b103a412696464839d5f8cd067657485
Edson Monteiro <edson.monteiro@chaordicsystems.com>
2017-08-22 11:57:17 -0300

    Adding Test checker

---
+++ src/Checkers/TesteChecker.php
@@ -0,0 +1,30 @@
+<?php
+namespace KR04\Checkers;
+
+use KR04\Checkers\Checker;
+use KR04\Files\Loader;
+use KR04\System\Colorize;
+
+class TesteChecker extends Checker
+{
+
+    public function __construct(Loader $loader)
+    {
+        parent::__construct($loader);
+    }
+
+    protected function check(): Checker
+    {
+        Colorize::show('[bg-blue][white]This is a new Checker test[/]');
+
+        return $this;
+    }
+
+    protected function configure(): Checker
+    {
+        /**
+         * Don't need implementation =)
+         */
+        return $this;
+    }
+}
