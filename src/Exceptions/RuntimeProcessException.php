543daee636b653bc76ee1c7087b430793046aa6d
Edson Monteiro <edson.monteiro@chaordicsystems.com>
2017-08-22 10:07:02 -0300

    Adding Exceptions of the application

---
+++ src/Exceptions/RuntimeProcessException.php
@@ -0,0 +1,19 @@
+<?php
+namespace KR04\Exceptions;
+
+use KR04\System\Colorize;
+
+class RuntimeProcessException extends \Exception
+{
+
+    public function __construct(string $message = "", int $code = 0, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+    
+    public function __toString()
+    {
+        return Colorize::color('[bg-white][cyan]' . __CLASS__ . "\n"
+            . '[bg-red][white][' . $this->code . ']: ' . $this->message . '[/]');
+    }
+}
