de79ffab970dcc35cce7e74263bf6edf11670807
Edson Monteiro <edson.monteiro@chaordicsystems.com>
2017-08-23 17:10:32 -0300

    Replace '$' to '$' into de message

--- src/System/Colorize.php
+++ src/System/Colorize.php
@@ -57,6 +57,8 @@ class Colorize
     private static function colorizeMsg(string $msg): string
     {
         $os = array_key_exists(PHP_OS, self::$colors) ? PHP_OS : 'OTHER';
+        
+        $msg = str_replace('$', '\$', $msg);
 
         foreach (self::$colors[$os] as $key => $value) {
             $msg = str_replace($key, $value, $msg);
