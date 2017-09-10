4021a4afbd382f9be42d3b78209e750362b8c10f
br-monteiro <edson.monteiro@chaordicsystems.com>
2017-09-09 23:29:25 -0300

    Refactoring the executable of system

--- index.php
+++ index.php
@@ -5,8 +5,14 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 
 try {
 
+    $checkerContainer = new KR04\Checkers\CheckerContainer();
+    // register the Checker here!
+    $checkerContainer->setChecker(\KR04\Checkers\SyntaxChecker::class);
+    $checkerContainer->setChecker(\KR04\Checkers\PsrChecker::class);
+    $checkerContainer->setChecker(\KR04\Checkers\ChaordicPatternChecker::class);
+
     // init the verification into the files
-    new KR04\Linter();
+    new KR04\Linter($checkerContainer);
 } catch (\Exception $ex) {
 
     \KR04\System\Colorize::show($ex);
